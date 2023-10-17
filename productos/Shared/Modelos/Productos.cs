f89fd2067d787a3a7798242f615c634962fca85f
maraq <maraq@PCMARA>
2023-10-17 15:02:30 -0600

    Agregar archivos de proyecto.

---
+++ productos/Shared/Modelos/Productos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productos.Shared.Modelos
+{
+    public class Productos
+    {
+        [Required(ErrorMessage = "El campo {0} es obbligatorio")]
+        [Range (1,100000000, ErrorMessage="Ingresa un número válido")]
+        public int? Inventario { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obbligatorio")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obbligatorio")]
+        public string? Departamento { get; set; }
+
+    }
+}
